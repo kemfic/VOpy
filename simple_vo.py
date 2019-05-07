460ca26a7058ab8ff79e3f7db97d632d6a5102fc
kemfic <19ficici.kemal@gmail.com>
2019-05-07 01:29:47 -0400

    updated simple_vo

--- simple_vo.py
+++ simple_vo.py
@@ -1,20 +1,24 @@
 import cv2
 import numpy as np
-from params import param
+from frame import Frame
 
 if __name__ == "__main__":
   cap = cv2.VideoCapture('vid/06.mp4')
   ret, frame = cap.read()
+  prevFrame = Frame(frame)
   while cap.isOpened():
     ret, frame = cap.read()
-    corners = getCorners(frame)
-    coords, des = get_features_orb(frame, corners)
     
-    #print(corners[0])
+    # Visual Odom Stuff Here
+    curFrame = Frame(frame)
+    curFrame.match(prevFrame)
+
     
-    [cv2.circle(frame, tuple(i.ravel()), 2, (0,255,0)) for i in corners]
-    [cv2.circle(frame, tuple(np.int0(i)), 2, (255,255,0)) for i in coords]
+    
+    
     cv2.imshow("frame", frame)
+    prevFrame = curFrame
+    
     if cv2.waitKey(1) & 0xFF == ord('q'):
       print("exiting...")
       break
