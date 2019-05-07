06386f56346a3e4715cbcf4824f0951cd809cf0c
kemfic <19ficici.kemal@gmail.com>
2019-05-06 23:57:55 -0400

    added Shi-Tomasi Feature Tracking

---
+++ params.py
@@ -0,0 +1,6 @@
+param = dict(
+  gftt = dict(maxCorners = 3000,
+              qualityLevel = 0.01,
+              minDistance = 10)
+              
+              )
--- simple_vo.py
+++ simple_vo.py
@@ -1,11 +1,33 @@
 import cv2
 import numpy as np
+from params import param
+
+def bgr2gray(img):
+  return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+def getCorners(img, params=param.get("gftt")):
+  """
+  Example from: https://docs.opencv.org/3.0-beta/doc/py_tutorials/py_feature2d/py_shi_tomasi/py_shi_tomasi.html
+  
+  API Ref: https://docs.opencv.org/master/dd/d1a/group__imgproc__feature.html#ga1d6bb77486c8f92d79c8793ad995d541
+  """
+
+
+  gray = bgr2gray(img)
+  corners =  cv2.goodFeaturesToTrack(gray, **params)
+  corners = np.int0(corners)
+  return corners
 
 if __name__ == "__main__":
   cap = cv2.VideoCapture('vid/06.mp4')
   ret, frame = cap.read()
   while cap.isOpened():
     ret, frame = cap.read()
+    corners = getCorners(frame)
+    print(corners[0])
+    
+    [cv2.circle(frame, tuple(i.ravel()), 3, 255, -1) for i in corners]
+
     cv2.imshow("frame", frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
       print("exiting...")
